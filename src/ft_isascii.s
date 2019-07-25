4370769b02ffeac85867338f97ef11ccb67842a5
Jean-Baptiste Terrazzoni <jterrazz@protonmail.com>
2019-07-25 13:52:43 +0200

    Fix values

--- src/ft_isascii.s
+++ src/ft_isascii.s
@@ -6,14 +6,14 @@
 ;    By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2019/07/04 21:54:43 by jterrazz          #+#    #+#              ;
-;    Updated: 2019/07/05 00:11:18 by jterrazz         ###   ########.fr        ;
+;    Updated: 2019/07/25 13:51:49 by jterrazz         ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
 global _ft_isascii
 
 _ft_isascii:
-	cmp rdi, 177
+	cmp rdi, 127
 	jg is_not_ascii
 
 is_ascii:
--- src/ft_isprint.s
+++ src/ft_isprint.s
@@ -6,16 +6,16 @@
 ;    By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2019/07/04 22:03:21 by jterrazz          #+#    #+#              ;
-;    Updated: 2019/07/05 00:11:30 by jterrazz         ###   ########.fr        ;
+;    Updated: 2019/07/25 13:52:16 by jterrazz         ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
 global _ft_isprint
 
 _ft_isprint:
-	cmp rdi, 40
+	cmp rdi, 32
 	jl is_not_print
-	cmp rdi, 164
+	cmp rdi, 126
 	jg is_not_print
 
 is_print:
